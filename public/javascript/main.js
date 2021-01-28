dedf104e1bd92be7b08e3f20846b2ee6462a5366
GAUTHIER <gauthierdewit@gmail.com>
2021-01-28 17:01:37 +0100

    exo_09

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,83 @@
 //les fonctions
 //----------------------------------
 // présentation
+/* 
+let direBonjour = (prenom, nom) => {
+    console.log(`Bonjour ${prenom} ${nom}!`);
+  };
+  direBonjour("Nathan", "Van Dyck");
+ */  
+//-----------------------------------
+//01
+/* 
+let addition = ( a , b ) =>{
+    return a + b 
+} 
+let resultat = addition ("5" , "2")
+console.log(resultat)
+ */
+//-----------------------------------
+//02
+/* 
+let soust = ( a , b ) =>{
+    return a - b 
+} 
+let resultat = soust ("5" , "2")
+console.log(resultat)
+ */
+//------------------------------------
+//03
+/* 
+let mult = ( a , b ) =>{
+    return a * b 
+} 
+let resultat = mult ("5" , "2")
+console.log(resultat)
+ */
+//-------------------------------------
+//04
+/* 
+let divi = ( a , b ) =>{
+    return a / b 
+} 
+let resultat = divi ("5" , "2")
+console.log(resultat)
+ */
+//------------------------------------
+//05
+/* 
+let mod = ( a , b ) =>{
+    return a % b 
+} 
+let resultat = mod ("5" , "2")
+console.log(resultat)
+ */
+//------------------------------------
+//06
+/* 
+let car = ( a ) =>{
+    return math.sqrt(a)
+} 
+let resultat = car ("5")
+console.log(resultat)
+ */
+//------------------------------------
+//07
+/* 
+let expo = ( a , b ) =>{
+    return a ** b 
+} 
+let resultat = expo ("5" , "2")
+console.log(resultat)
+ */
+//------------------------------------
+//08
+/* 
+let capitalize = ( a ) =>{
+    return a.charAt(0).toUpperCase() + a.slice(1).toLowerCase() 
+} 
+console.log (capitalize (prompt (" donne moi un mot ")))
+ */
+//-------------------------------------
+//09
+
